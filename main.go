35fa2f3e07e79784188872675f5a3f4273c31fbd
Otávio Alves <otavioaugusto813@hotmail.com>
2024-05-20 16:49:40 -0300

    Criando rotas

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/otavioaugusto813/go-api/router"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	router.Initialize()
+	// router.Run() // listen and serve on 0.0.0.0:8080
 }
